ef9b6a1dde3692e07be08f9b872a01476af8ad93
Jon Higgins <jon@jonhiggins.co.uk>
2021-12-07 17:46:10 +0000

    Add date validation

--- src/server/routes/getUserDate.ts
+++ src/server/routes/getUserDate.ts
@@ -1,40 +1,48 @@
+import { isMatch } from 'date-fns'
 import { Request, Response } from 'express'
 import db from '../db'
+
+async function getValues(userIdInt: number, date: string) {
+    const query = `SELECT 
+    track.title as track,
+    artist.title as artist
+FROM day_item
+INNER JOIN track ON track.id = track_id
+INNER JOIN artist ON artist.id = artist_id
+WHERE day_item.user_id = $1
+AND date = $2`
+
+    try {
+        const result = await db.query(query, [userIdInt, date])
+        return { result, error: null }
+    } catch (error) {
+        return { result: null, error }
+    }
+}
 
 export default async function getUserDate(
     { params: { userId, date } }: Request,
     response: Response
 ) {
     const userIdInt = parseInt(userId, 10)
+    const isValidDate = isMatch(date, 'yyyy-MM-dd') && date.length === 10
+
     if (!userId || !userIdInt) {
         return response.status(500).json({ error: 'Invalid userId' })
     }
-    if (!date) {
-        // @todo date validaton
+    if (!date || !isValidDate) {
         return response.status(500).json({ error: 'Invalid date' })
     }
-    let rows = null
-    try {
-        const result = await db.query(
-            `SELECT 
-            track.title as track,
-            artist.title as artist
-        FROM day_item
-        INNER JOIN track ON track.id = track_id
-        INNER JOIN artist ON artist.id = artist_id
-        WHERE day_item.user_id = $1
-        AND date = $2
-        `,
-            [userId, date]
-        )
-        rows = result.rows
-    } catch (error) {
-        return response.status(500).json(error)
+
+    const { result, error } = await getValues(userIdInt, date)
+
+    if (error) {
+        return response.status(500).json({ error })
     }
-    if (!rows || rows.length === 0) {
+    if (!result || !result.rows || result.rows.length === 0) {
         return response
             .status(404)
             .json({ error: 'Could not find user or tracks' })
     }
-    return response.status(200).json(rows)
+    return response.status(200).json(result.rows)
 }
--- src/server/sql/songs-for-day-query.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-	track.title,
-	artist.title
-FROM day_item
-INNER JOIN track ON track.id = track_id
-INNER JOIN artist ON artist.id = artist_id
-WHERE day_item.user_id = 1
-AND date = '2021-12-06'
